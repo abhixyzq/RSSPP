a43651175b8c20f0fdcffdbe08b49bee7ef7f1f8
Abhishek Kumar <AbhiXYZQ@users.noreply.github.com>
2026-07-01 11:24:43 +0530

    Revert admin login and forgot password spacing back to centered

--- src/app/admin-login/forgot-password/page.tsx
+++ src/app/admin-login/forgot-password/page.tsx
@@ -90,7 +90,8 @@ export default function ForgotPasswordPage() {
         </div>
       </nav>
 
-      <main className="relative z-10 max-w-7xl mx-auto px-6 lg:px-12 flex-1 flex flex-col justify-start lg:justify-center pt-6 lg:pt-0 pb-8 lg:py-0 lg:min-h-0 w-full">
+      {/* Hero Section */}
+      <main className="relative z-10 max-w-7xl mx-auto px-6 lg:px-12 flex-1 flex flex-col justify-center py-8 lg:py-0 lg:min-h-0 w-full">
         
         {/* Centered Recovery Component */}
         <div className="relative w-full flex items-center justify-center">
